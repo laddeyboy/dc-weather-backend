08cec7307a5e674fc396a0159db9d2b0d325ad21
Paul Bailey <paul.m.bailey@gmail.com>
2018-08-18 17:48:17 -0700

    initial setup

---
+++ app.js
@@ -0,0 +1,32 @@
+var axios = require('axios');
+var express = require('express');
+var morgan = require('morgan');
+
+var env_result = require('dotenv').config();
+if (env_result.error && env_result.error.code != 'ENOENT') {
+  throw env_result.error;
+}
+
+var app = express();
+app.use(morgan('dev'));
+
+app.get("/", function (request, response, next) {
+  var zip = request.query.zip || "78133";
+
+  axios.get(
+    'http://api.openweathermap.org/data/2.5/weather',
+    {params: {q: zip, APPID: process.env.OPEN_WEATHER_MAP_KEY}}
+  )
+    .then((result) => {
+      var temp = Math.round(1.8 * (result.data.main.temp - 273) + 32);
+
+      response.json({temp: temp});
+    })
+    .catch(next);
+});
+
+var PORT = process.env.PORT || 9000;
+
+app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+});
